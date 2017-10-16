e03c7fc51c32009eaf13849c2f54f1455aa76053
Matthew Segal <mattdsegal@gmail.com>
2017-10-16 13:01:16 +1100

    tiny clean up

--- people/admin.py
+++ people/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 
 from .models import Candidate
 
-# make UUID read only
-# Add 'optional' to is_citizen
+
 class CandidateAdmin(admin.ModelAdmin):
   readonly_fields = ('uuid',)
 
